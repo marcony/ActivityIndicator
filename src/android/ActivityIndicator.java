39ce6caf9d860098094e0eee82a1c88bd516686e
Marcony <marconypessotti@gmail.com>
2014-07-22 15:03:43 -0300

    fix threading

--- src/android/ActivityIndicator.java
+++ src/android/ActivityIndicator.java
@@ -207,38 +207,38 @@ public class ActivityIndicator extends CordovaPlugin {
 			stopWorker = false;
 			readBufferPosition = 0;
 			readBuffer = new byte[1024];
-// 			workerThread = new Thread(new Runnable() {
-// 				public void run() {
-// 					while (!Thread.currentThread().isInterrupted() && !stopWorker) {
-// 						try {
-// 							int bytesAvailable = mmInputStream.available();
-// 							if (bytesAvailable > 0) {
-// 								byte[] packetBytes = new byte[bytesAvailable];
-// 								mmInputStream.read(packetBytes);
-// 								for (int i = 0; i < bytesAvailable; i++) {
-// 									byte b = packetBytes[i];
-// 									if (b == delimiter) {
-// 										byte[] encodedBytes = new byte[readBufferPosition];
-// 										System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-// //										final String data = new String(encodedBytes, "US-ASCII");
-// //										readBufferPosition = 0;
-// //										handler.post(new Runnable() {
-// //											public void run() {
-// //												myLabel.setText(data);
-// //											}
-// //										});
-// 									} else {
-// 										readBuffer[readBufferPosition++] = b;
-// 									}
-// 								}
-// 							}
-// 						} catch (IOException ex) {
-// 							stopWorker = true;
-// 						}
-// 					}
-// 				}
-// 			});
-// 			workerThread.start();
+			workerThread = new Thread(new Runnable() {
+				public void run() {
+					while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+						try {
+							int bytesAvailable = mmInputStream.available();
+							if (bytesAvailable > 0) {
+								byte[] packetBytes = new byte[bytesAvailable];
+								mmInputStream.read(packetBytes);
+								for (int i = 0; i < bytesAvailable; i++) {
+									byte b = packetBytes[i];
+									if (b == delimiter) {
+										byte[] encodedBytes = new byte[readBufferPosition];
+										System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+//										final String data = new String(encodedBytes, "US-ASCII");
+//										readBufferPosition = 0;
+//										handler.post(new Runnable() {
+//											public void run() {
+//												myLabel.setText(data);
+//											}
+//										});
+									} else {
+										readBuffer[readBufferPosition++] = b;
+									}
+								}
+							}
+						} catch (IOException ex) {
+							stopWorker = true;
+						}
+					}
+				}
+			});
+			workerThread.start();
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
